db90d1a34c0b55b0c22101cc2df706be0d2e36e5
maestro2506 <miras.b2506@gmail.com>
2019-10-17 00:16:23 +0600

    Data base connection now work properly DONT FORGET TO USE YOUR DATA IN application.properties in resources directory

---
+++ src/main/java/kz/edu/nu/cs/se/Servlets/SpringTest.java
@@ -0,0 +1,11 @@
+package kz.edu.nu.cs.se.Servlets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTest {
+    public static void main (String[] args){
+        SpringApplication.run(SpringTest.class, args);
+    }
+}
